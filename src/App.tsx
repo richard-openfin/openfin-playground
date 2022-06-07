dd542bc274cfdd74a369c3b17a314649001e934f
Richard Gutan <richard.gutan@openfin.co>
2022-06-07 13:04:44 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { Fin } from 'openfin-adapter';
+
+declare const fin: Fin;
+
 function App() {
+    useEffect(() => {
+        const init = async () => {
+            await fin.Platform.init();
+        }
+        init();
+        
+    }, []);
+
     return <div>Playground</div>;
 }
 
