80db75b6b785cd59286ee19870220a148cd8dae8
Richard Gutan <richard.gutan@openfin.co>
2022-08-26 13:00:27 +0100

    changes

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 1. `npm i`
 2. `npm start`
 
-## TBD
+## How to reproduce the issue
+
+1. Have 2 monitors
+2. Have primary monitor positioned above secondary monitor
+3. Have them scaled differently i.e. primary monitor at 150% and secondary monitor at 100%
+4. Start the app and keep generating windows until you see anomalies in their size. Expected behaviour is to have all generated windows with same size.
+
+Important thing here is that the window must be initially created outside of the primary monitor bounds but only if they're created where they're overlapping secondary monitor with different scale.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,70 @@
+import { fin } from "@openfin/core";
+import { useEffect, useState } from "react";
+
+const windowOptions = {
+    defaultHeight: 92,
+    defaultWidth: 312,
+    resizable: false,
+    shadow: true,
+    saveWindowState: false,
+    frame: false,
+    alwaysOnTop: true,
+    showTaskbarIcon: false,
+    opacity: 1,
+    backgroundColor: "#2F3136",
+};
+
 function App() {
-    return <div>Playground</div>;
+    const [windows, setWindows] = useState<OpenFin.Window[]>([]);
+
+    const create = async () => {
+        const {
+            primaryMonitor: {
+                monitorRect: { bottom, right },
+            },
+        } = await fin.System.getMonitorInfo();
+
+        const win = await fin.Window.create({
+            ...windowOptions,
+            name: `toast:${Date.now()}`,
+            defaultTop: bottom + windowOptions.defaultHeight,
+            defaultLeft: right + windowOptions.defaultWidth,
+        });
+
+        await win.animate(
+            {
+                position: {
+                    left: Math.floor(Math.random() * right),
+                    top: Math.floor(Math.random() * bottom),
+                    duration: 100,
+                },
+            },
+            {
+                interrupt: true,
+                tween: "linear",
+            }
+        );
+
+        setWindows((currWindows) => [...currWindows, win]);
+    };
+
+    const closeAll = async () => {
+        await Promise.all(windows.map((w) => w.close()));
+        setWindows([]);
+    };
+
+    useEffect(() => {
+        return () => {
+            closeAll();
+        };
+    }, []);
+
+    return (
+        <div>
+            Playground<button onClick={create}>Create Window</button>
+            <button onClick={closeAll}>Close All</button>
+        </div>
+    );
 }
 
 export default App;
