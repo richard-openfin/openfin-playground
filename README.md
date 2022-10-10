a078fb5206e115e7fa8574e50f6bdff5d3caa1a4
Richard Gutan <richard.gutan@openfin.co>
2022-10-10 18:21:40 +0100

    added sample

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## Getting Started
 
-1. `npm i`
-2. `npm start`
+Simple app to test if client libraries work as expected.
 
-## TBD
+1. `npm run clean` to make sure there are not packages from previous versions
+2. Install client libraries and specify desired version to test i.e. `npm i @openfin/workspace@10.1.0 @openfin/workspace-platform@10.1.0 openfin-notifications@1.17.2-alpha-1368`
+3. Set the desired runtime version in `public/app.json`
+4. `npm start` and watch if there are errors
+5. Once app starts, check the console for errors
+6. Try sample APIs by pressing respective buttons and see if all are successful
+7. If there are no errors in steps 2, 4, 5, 6 we're good to go :-)
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,84 @@
+import * as notifications from "openfin-notifications";
+import * as workspacePlatform from "@openfin/workspace-platform";
+import * as workspace from "@openfin/workspace";
+import { useState } from "react";
+
+const getStatus = (status: boolean | null) => {
+    if (status === null) {
+        return "";
+    }
+    return status ? "success" : "error";
+};
+
+const getStatusColor = (status: boolean | null) => (status ? "green" : "red");
+
 function App() {
-    return <div>Playground</div>;
+    const [nfnsResponse, setNfnsResponse] = useState<boolean | null>(null);
+    const [wpResponse, setWpResponse] = useState<boolean | null>(null);
+    const [wsResponse, setWsResponse] = useState<boolean | null>(null);
+
+    const nfns = async () => {
+        try {
+            await notifications.provider.getStatus();
+            setNfnsResponse(true);
+        } catch (err) {
+            console.error(err);
+            setNfnsResponse(false);
+        }
+    };
+
+    const wp = async () => {
+        try {
+            await workspacePlatform.init({});
+            setWpResponse(true);
+        } catch (err) {
+            console.error(err);
+            setWpResponse(false);
+        }
+    };
+
+    const ws = async () => {
+        try {
+            await workspace.Home.register({
+                id: "test-home",
+                title: "Test Home",
+                icon: "https://google.com/favicon.ico",
+                onUserInput: async () => ({ results: [] }),
+            });
+            setWsResponse(true);
+        } catch (err) {
+            console.error(err);
+            setWsResponse(false);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={nfns}>Notifications.getStatus()</button>
+            <button onClick={wp}>WorkspacePlatform.init()</button>
+            {wpResponse && (
+                <button onClick={ws}>Workspace.Home.register()</button>
+            )}
+            <p>
+                Notifications.getStatus() response:{" "}
+                <span style={{ color: getStatusColor(nfnsResponse) }}>
+                    {getStatus(nfnsResponse)}
+                </span>
+            </p>
+            <p>
+                WorkspacePlatform.init() response:{" "}
+                <span style={{ color: getStatusColor(wpResponse) }}>
+                    {getStatus(wpResponse)}
+                </span>
+            </p>
+            <p>
+                Workspace.Home.register() response:{" "}
+                <span style={{ color: getStatusColor(wsResponse) }}>
+                    {getStatus(wsResponse)}
+                </span>
+            </p>
+        </div>
+    );
 }
 
 export default App;
