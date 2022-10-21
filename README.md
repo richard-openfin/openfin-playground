3f22e5df05b647c0442f1d2415c0f2e91ae9d12e
Richard Gutan <richard.gutan@openfin.co>
2022-10-21 15:53:51 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-## Getting Started
+## Steps to reproduce
 
 1. `npm i`
 2. `npm start`
-
-## TBD
+3. Make sure system text size is not set i.e.
+4. ![Text size not set](public/img1.png)
+5. Press the animate button several times.
+6. Observe that animation is correct.
+7. Set system text size i.e.
+8. ![Text size set](public/img2.png)
+9. Press the animate button several times.
+10. See that window escapes the screen
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { fin } from "@openfin/core";
+
+const defaultSize = {
+    width: 800,
+    height: 500,
+};
+
+const newSize = {
+    width: 500,
+    height: 300,
+};
+
 function App() {
-    return <div>Playground</div>;
+    const animate = async () => {
+        const win = fin.Window.wrapSync(fin.me.identity);
+        const { width, height } = await fin.me.getBounds();
+        await win.animate(
+            {
+                size: {
+                    width:
+                        width === defaultSize.width
+                            ? newSize.width
+                            : defaultSize.width,
+                    height:
+                        height === defaultSize.height
+                            ? newSize.height
+                            : defaultSize.height,
+                    duration: 150,
+                },
+            },
+            { interrupt: true }
+        );
+    };
+
+    return (
+        <div>
+            <button onClick={animate}>animate</button>
+        </div>
+    );
 }
 
 export default App;
