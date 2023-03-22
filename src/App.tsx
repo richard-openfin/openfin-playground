285dbc9f502628aaa0e080ec039431c48a90c4d3
Richard Gutan <richard.gutan@openfin.co>
2023-03-22 12:25:29 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import { init, getCurrentSync } from "@openfin/workspace-platform";
+import { Dock } from "@openfin/workspace";
+
 function App() {
+    useEffect(() => {
+        init({});
+
+        getCurrentSync().on("platform-api-ready", () => {
+            Dock.register({
+                id: "test-dock",
+                title: "Test Dock",
+                icon: "https://openfin.co/favicon.ico",
+            })
+                .then(() => {
+                    console.log("Dock.register promise resolved");
+                })
+                .catch(() => {
+                    console.log("Dock.register promise rejected");
+                });
+        });
+    }, []);
+
     return <div>Playground</div>;
 }
 
