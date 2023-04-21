c9d3e7ca4c229d3a2028eab1d9599ac2716f7ba2
Richard Gutan <richard.gutan@openfin.co>
2023-04-21 17:52:48 +0200

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import {} from "@openfin/workspace";
+import * as WP from "@openfin/workspace-platform";
+import { useEffect } from "react";
+
+const samplePage = {
+    title: "Test",
+    pageId: "testpage",
+    layout: {
+        content: [
+            {
+                type: "stack",
+                content: [
+                    {
+                        type: "component",
+                        componentName: "view",
+                        componentState: {
+                            url: "https://examples.com",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
+
 function App() {
-    return <div>Playground</div>;
+    useEffect(() => {
+        const init = async () => {
+            await WP.init({ browser: {} });
+            const platform = WP.getCurrentSync();
+            platform.Browser.createWindow({
+                workspacePlatform: {
+                    pages: [samplePage],
+                },
+            });
+        };
+        init();
+    }, []);
+
+    return <div>Playground 2</div>;
 }
 
 export default App;
