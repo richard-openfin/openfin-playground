d57dd0377afaa2a54f83bf8ae73841b663b95ecd
Richard Gutan <richard.gutan@openfin.co>
2023-10-10 11:29:10 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { fin } from "@openfin/core";
+
 function App() {
+    useEffect(() => {
+        const init = async () => {
+            const win = await fin.Window.create({
+                name: "test",
+                hotkeys: [{ keys: "Ctrl+M" }],
+            });
+
+            win.addListener("hotkey", (event) => {
+                console.log(111, event);
+            });
+        };
+
+        init();
+    }, []);
     return <div>Playground</div>;
 }
 
